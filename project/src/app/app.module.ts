a6721551c774215e2fa865cc031fba71ea8763f2
gasperapi <gasperapi@gmail.com>
2022-05-02 11:26:26 +0700

    add 2 component user and navbar

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
